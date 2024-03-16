6adc4e696e8379f18e2a8bf57b3b9735064436ab
Gerardo Vazquez <neowhatup@gmail.com>
2024-03-15 21:38:14 -0600

    confirmado

---
+++ 1_edicion/06-definicion.js
@@ -0,0 +1,17 @@
+//*
+//*    Objetivo:
+//*        Ir a la definición de la función saludar rápidamente
+//*
+//*    Tips:
+//*        Ojear definición   ⌥ F12
+//*        Ir a la definición F12
+//*
+//*        Ojear definición   Alt + F12
+//*        Ir a la definición F12
+//*
+
+import { saludar } from '1_edicion/extra/funciones.js';
+
+const saludo = saludar( 'Thanos' );
+
+console.log(saludo);
---
+++ 1_edicion/extra/funciones.js
@@ -0,0 +1,90 @@
+
+
+
+export function saludar( nombre ) {
+    console.log( `${nombre}Yo no hago nada!!!`);
+}
+
+export function saludar2(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar3(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar4(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar5(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar6(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+// Este es un problema
+export function saludar7(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar8(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar9(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar10(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar11(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar12(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar13(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar14(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar15(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar16(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar( nombre ) {
+    return `Hola ${ nombre }!!!`;
+}
+
+export function saludar17(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar18(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar19(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+export function saludar20(  ) {
+    return `Yo no hago nada!!!`;
+}
+
+
+
